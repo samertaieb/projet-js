59bf052fb507b221516945842e32555ff186dca8
samertaieb <taieb.samer@outlook.fr>
2020-11-26 10:40:24 +0100

    sign up and log in

---
+++ project js/logIn.js
@@ -0,0 +1,19 @@
+function seConnecter(){
+    const users=JSON.parse(localStorage.getItem(users))
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    if(email!="" || password!=""){
+        const user =users.find(e=>e.email==email && e.password==password )
+        if(user!=undefined){
+            localStorage.setItem("user",JSON.stringify(user));
+            window.location.replace("")
+        }
+        else {
+            alert("L'adresse email est déja existe")
+        }
+    }
+    else{
+        alert("remplissez tous les champs")
+    }
+}
+fu
