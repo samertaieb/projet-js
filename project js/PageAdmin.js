f90fe22806fc04c40239ae7c697e6c5f40b6114e
samertaieb <taieb.samer@outlook.fr>
2020-11-30 14:57:11 +0100

    modification liste candidats

--- project js/PageAdmin.js
+++ project js/PageAdmin.js
@@ -1,4 +1,5 @@
-var tableau_candidats = JSON.parse(localStorage.getItem("tableau_candidats")) || [];
+var tableau_candidats =
+  JSON.parse(localStorage.getItem("tableau_candidats")) || [];
 
 var ajouter_candidat = document.getElementById("ajouter_candidat");
 
@@ -8,7 +9,12 @@ function ajout_candidats() {
   const prenom_candidat = document.getElementById("prenom_candidat").value;
   const numero_candidat = document.getElementById("numero_candidat").value;
   const email_candidat = document.getElementById("email_candidat").value;
-  if (nom_candidat != "" && prenom_candidat != "" && email_candidat != "" && numero_candidat != "") {
+  if (
+    nom_candidat != "" &&
+    prenom_candidat != "" &&
+    email_candidat != "" &&
+    numero_candidat != ""
+  ) {
     var candidatObj = {
       nom: nom_candidat,
       prenom: prenom_candidat,
@@ -18,7 +24,10 @@ function ajout_candidats() {
       idEtablissement: "",
     };
     tableau_candidats.push(candidatObj);
-    localStorage.setItem("tableau_candidats", JSON.stringify(tableau_candidats));
+    localStorage.setItem(
+      "tableau_candidats",
+      JSON.stringify(tableau_candidats)
+    );
     // const alert11 = document.getElementById("alert_candidat").innerHTML = "Candidat ajouté avec succée"
     document.getElementById("nom_candidat").value = "";
     document.getElementById("prenom_candidat").value = "";
@@ -35,7 +44,7 @@ function ajout_candidats() {
 const tbody = document.getElementById("tbody_liste");
 function liste_candidats() {
   var candidats = JSON.parse(localStorage.getItem("tableau_candidats")) || [];
-  candidats.forEach((candidatObj) => {
+  candidats.forEach(candidatObj => {
     tbody.innerHTML += ` <tr id="${candidatObj.id}">
     <th scope="col">${candidatObj.id}</th>
     <th scope="col">${candidatObj.nom}</th>
@@ -43,20 +52,87 @@ function liste_candidats() {
     <th scope="col">${candidatObj.email}</th>
     <th scope="col">${candidatObj.numero}</th>
    <th scope="col">
-   <button type="button" class="btn btn-warning">Modifier</button>
+   <!-- Button trigger modal -->
+   <button type="button" class="btn btn-primary modifier" data-toggle="modal" data-target="#exampleModal" >
+modifier</button>
+   
    <button  type="button" class="btn btn-danger delete">Supprimer</button>
    </th>
 </tr>`;
   });
 }
+const newNomCandidat = document.getElementById("newNomCandidat");
+const newPrenomCandidat = document.getElementById("newPrenomCandidat");
+const newEmailCandidat = document.getElementById("newEmailCandidat");
+const newNumeroCandidat = document.getElementById("newNumeroCandidat");
+let idCandidat;
 
-tbody.addEventListener("click", (e) => {
+tbody.addEventListener("click", e => {
+  const candidats = JSON.parse(localStorage.getItem("tableau_candidats"));
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.parentElement.remove();
-    const candidats = JSON.parse(localStorage.getItem("tableau_candidats"));
     const filteredCandisdats = candidats.filter(
-      (candidatObj) => candidatObj.id != e.target.parentElement.parentElement.id
+      candidatObj => candidatObj.id != e.target.parentElement.parentElement.id
     );
-    localStorage.setItem("tableau_candidats", JSON.stringify(filteredCandisdats));
+    localStorage.setItem(
+      "tableau_candidats",
+      JSON.stringify(filteredCandisdats)
+    );
+  }
+  if (e.target.classList.contains("modifier")) {
+    const mofifierCandidat = candidats.find(
+      candidatObj => candidatObj.id == e.target.parentElement.parentElement.id
+    );
+    idCandidat = e.target.parentElement.parentElement.id;
+    newNomCandidat.value = mofifierCandidat.nom;
+    newPrenomCandidat.value = mofifierCandidat.prenom;
+    newEmailCandidat.value = mofifierCandidat.email;
+    newNumeroCandidat.value = mofifierCandidat.numero;
   }
 });
+// console.log(mofifierCandidat);
+
+// const user = JSON.parse(localStorage.getItem("user")) || [];
+const candidats = JSON.parse(localStorage.getItem("tableau_candidats"));
+const update = document.querySelector("#update");
+
+update.addEventListener("click", () => {
+  const modifiedCandidats = {
+    nom: newNomCandidat.value,
+    prenom: newPrenomCandidat.value,
+    email: newEmailCandidat.value,
+    numero: newNumeroCandidat.value,
+    id: idCandidat,
+  };
+
+  const newCandidat = candidats.map(candidat => {
+    if (candidat.id == modifiedCandidats.id) {
+      candidat = modifiedCandidats;
+    }
+    return candidat;
+  });
+  console.log(newCandidat);
+  localStorage.setItem("tableau_candidats", JSON.stringify(newCandidat));
+  alert((MSG = "Vous avez modifier vos données avec succées"));
+  MSG.color = "green";
+});
+
+// function modificationCandidats() {
+//   const modifiedCandidats = {
+//     nom_candidat: newNomCandidat.value,
+//     prenom_candidat: newPrenomCandidat.value,
+//     email_candidat: newEmailCandidat.value,
+//     numero_candidat: newNumeroCandidat.value,
+//     id: idCandidat,
+//   };
+//   localStorage.setItem("candidats", JSON.stringify(modifiedCandidats));
+//   const newCandidat = candidats.map(candidat => {
+//     if (candidat.id === id.modifiedCandidats) {
+//       candidat = modifiedCandidats;
+//     }
+//     return candidat;
+//   });
+//   localStorage.setItem("newCandidat", JSON.stringify(newCandidat));
+//   alert((MSG = "Vous avez modifier vos données avec succées"));
+//   MSG.color = "green";
+// }
